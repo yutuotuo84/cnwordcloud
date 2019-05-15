518ff08fbb0de5d0938bac27226fc4fadf6abb8c
yutuotuo84 <yutuotuo84@126.com>
2019-05-15 19:05:25 +0800

    first commit. Version 0.1

---
+++ R/cnwordcloud.R
@@ -0,0 +1,33 @@
+#' This function allows you to generate a wordcloud in Chinese.
+#' @name cnwordcloud
+#' @param text a vector of strings. 
+#' @param stopwords a dictionary of stopwords, default to cn_stopwords.
+#' @param nwords the number of words to include in the wordcloud, default to 50.
+#' @return none.
+#' @keywords wordcloud, Chinese.
+#' @examples 
+#' library(cnwordcloud)
+#' data(sample_text)
+#' cnwordcloud(sample_text)
+#' @export
+cnwordcloud <- function(text, stopwords=NULL, nwords=50){
+  # segment text 
+  if(is.null(stopwords)){
+    stopwords <- data(cn_stopwords, envir = environment())
+  }
+  words <- jiebaR::qseg[text]
+  # remove stopwords 
+  words <- words[!(words %in% stopwords)]
+  # remove numbers 
+  words <- words[!(grepl("^[0-9]+$", words))]
+  # calculate word frequency 
+  freq <- table(words)
+  # sort the frequency table in descending order
+  freq <- sort(freq, decreasing = TRUE)
+  # convert the frequency table to DataFrame 
+  freq_table <- data.frame(freq)
+  # draw wordcloud 
+  wordcloud2::wordcloud2(freq_table[1:nwords,])
+  
+  
+}
---
+++ R/data.R
@@ -0,0 +1,16 @@
+#'1893 Chinese stopwords 
+#'
+#'@format a string vector
+#'@source \url{https://blog.csdn.net/shijiebei2009/article/details/39696571"}
+#'
+#'
+"cn_stopwords"
+#'
+#'
+#'
+#'A sample text, Xi's speech at 2019 CDAC 
+#'@format a string vector
+#'@source \url{https://news.sina.com.cn/gov/xlxw/2019-05-15/doc-ihvhiqax8868898.shtml}
+#'
+#'
+"sample_text"
